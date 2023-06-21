aa16ebf5632a2b85f01578f860a4a6f48e8275d1
Laura-D-Andrews <ldandrews88@gmail.com>
2023-06-21 13:23:39 -0400

    pillow install

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from datetime import datetime
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class User(AbstractUser):
+    avatar_img = models.ImageField()
+    bio = models.TextField()
+    birth_date = models.DateField()
+    follows = models.ManyToManyField('self')
+
+
+class Card(models.Model):
+    PRIVACY_CHOICES = [(False, 'Private'), (True, 'Public')]
+
+    sender = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name='cards_to_sender')
+    receiver = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name='cards_to_receiver')
+    text = models.CharField(max_length=300)
+    image_urls = models.URLField(max_length=500)
+    date_created = models.DateTimeField()
+    privacy = models.BooleanField(default=True, choices=PRIVACY_CHOICES)
+    header = models.CharField(max_length=300)
+    front_text = models.TextField()
+    back_text = models.TextField()
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
