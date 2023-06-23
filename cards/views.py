489b97f87632bb6a5e044179892e7460b21cdca7
Exo30 <mstucke2@ncsu.edu>
2023-06-23 14:25:25 -0400

    what we got so far

--- cards/views.py
+++ cards/views.py
@@ -1,20 +1,40 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins, generics, permissions
 from .models import User, Card
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework import permissions
-from .serializers import UserSerializer, CardSerializer
+from rest_framework.views import APIView
+from .serializers import ProfileSerializer, CardSerializer
 
 # Create your views here.
 
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
+class ProfileViewSet(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-class CardViewSet(viewsets.ModelViewSet):
+class CardViewSet(generics.ListCreateAPIView, mixins.DestroyModelMixin, mixins.UpdateModelMixin):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
     permission_classes = [permissions.IsAuthenticated]
     
+class UserSentViewSet(generics.ListAPIView):
+    queryset = Card.objects.all()
+    def get_queryset(self):
+        return self.request.user.sent_by_user
+    serializer_class = CardSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class UserReceivedViewSet(generics.ListAPIView):
+    queryset = Card.objects.all()
+    def get_queryset(self):
+        return self.request.user.sent_to_user
+    serializer_class = CardSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class FollowerPostViewSet(generics.ListAPIView):
+    queryset = Card.objects.all()
+    def get_queryset(self):
+        return self.request.user.sent_by_user
+    serializer_class = CardSerializer
+    permission_classes = [permissions.IsAuthenticated]
