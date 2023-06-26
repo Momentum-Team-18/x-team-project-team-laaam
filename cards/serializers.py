1a8d6404158cc6986cefb7bf9ed7f68d72028926
Exo30 <mstucke2@ncsu.edu>
2023-06-26 11:04:35 -0400

    flushed migrations to get this to work

--- cards/serializers.py
+++ cards/serializers.py
@@ -4,26 +4,25 @@ from cards.models import Card, User, Follow
 
 
 class CardSerializer(serializers.ModelSerializer):
-
-    sent_by_user = serializers.ReadOnlyField(source='sent_by_user.username')
-    sent_to_user = serializers.ReadOnlyField(source='sent_to_user.username')
-
     class Meta:
         model = Card
         fields = '__all__'
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    followers = serializers.HyperlinkedRelatedField(
-        many=True, view_name='user_followers', read_only=True)
-
     class Meta:
         model = User
         exclude = ['password']
 
 
-class FollowSerializer(serializers.ModelSerializer):
-
+class ThisUserFollowsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Follow
-        fields = '__all__'
+        fields = ['user_this_user_is_following']
+
+
+class FollowsThisUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Follow
+        fields = ['this_user']
+
--- cards/views.py
+++ cards/views.py
@@ -4,7 +4,7 @@ from .models import User, Card, Follow
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import ProfileSerializer, CardSerializer, FollowSerializer
+from .serializers import FollowsThisUserSerializer, ThisUserFollowsSerializer, ProfileSerializer, CardSerializer
 
 # Create your views here.
 
@@ -15,31 +15,10 @@ class ProfileViewSet(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
 
-class AllCardViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class CardViewSet(generics.ListCreateAPIView, mixins.DestroyModelMixin, mixins.UpdateModelMixin):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
     permission_classes = [permissions.IsAuthenticated]
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, pk, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class OneCardViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
-    queryset = Card.objects.all()
-    serializer_class = CardSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, pk, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
 
 
 class UserSentViewSet(generics.ListAPIView):
@@ -60,19 +39,19 @@ class UserReceivedViewSet(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
 
-class UserFollowersViewSet(generics.ListAPIView):
+class ThisUserFollowsViewSet(generics.ListAPIView):
     queryset = Follow.objects.all()
 
-    # def get_queryset(self):
-    #     return self.request.user.follows_as_follower
-    serializer_class = FollowSerializer
-    # permission_classes = [permissions.IsAuthenticated]
+    def get_queryset(self):
+        return self.request.user.follows_user
+    serializer_class = ThisUserFollowsSerializer
 
 
-# class FollowerPostViewSet(generics.ListAPIView):
-#     queryset = Card.objects.all()
+class FollowsThisUserViewSet(generics.ListAPIView):
+    queryset = Follow.objects.all()
 
-#     def get_queryset(self):
-#         return self.request.user.sent_by_user
-#     serializer_class = CardSerializer
-#     permission_classes = [permissions.IsAuthenticated]
+    def get_queryset(self):
+        user = self.request.user
+        return Follow.objects.filter(user_this_user_is_following_id=user)
+    serializer_class = FollowsThisUserSerializer
+
